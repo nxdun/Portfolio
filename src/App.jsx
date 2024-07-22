5f33ea899f89356e4e7da6d5b164c98d4e5f9b0c
Nadu_Dev <inbox.nadun@gmail.com>
2024-07-22 21:31:50 +0530

    basic structure with ui added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
+import { Navbar, Hero, Projects, Contact, Footer } from "./sections";
+
 export default function App() {
   return (
-    <d className="maindiv">
-      <section className="">
-        <p></p>
-      </section>
-    </d>
-
-  )
+    <main className="flex flex-col min-h-screen">
+      <Navbar />
+      <Hero />
+      <Projects />
+      <Contact />
+      <Footer />
+    </main>
+  );
 }
