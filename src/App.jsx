15ee15b1881dc8f52ddcbbdd70289b7413d83aa3
Nadu_Dev <inbox.nadun@gmail.com>
2025-01-02 10:11:42 +0530

    chore : Dependancy Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import { Navbar, Hero, Projects, Contact, Footer } from "./sections";
-import { ShaderGradientCanvas, ShaderGradient } from "shadergradient";
+import { ShaderGradientCanvas, ShaderGradient } from "@shadergradient/react";
 import * as reactSpring from "@react-spring/three";
 import * as drei from "@react-three/drei";
 import * as fiber from "@react-three/fiber";
 import {  useScroll, useTransform } from "framer-motion";
 import BackToTop from "./components/BackToTop.jsx";
-import '@fontsource/roboto'; //TODO: chane roboto font to a good one
+import '@fontsource/roboto'; //TODO: change roboto font to a good one
 
 
 export default function App() {
@@ -29,7 +29,7 @@ export default function App() {
       >
         <ShaderGradient
           control="query"
-          urlString="https://www.shadergradient.co/customize?animate=on&axesHelper=off&bgColor1=%23000000&bgColor2=%23000000&brightness=&cAzimuthAngle=180&cDistance=3.6&cPolarAngle=90&cameraZoom=1&color1=%23ff0700&color2=%233d9fdb&color3=%23d0bce1&embedMode=off&envPreset=city&fov=45&gizmoHelper=hide&grain=on&lightType=3d&pixelDensity=1&positionX=-1.4&positionY=0&positionZ=0&reflection=0.1&rotationX=0&rotationY=10&rotationZ=50&shader=defaults&type=waterPlane&uDensity=1.3&uFrequency=5.5&uSpeed=0.4&uStrength=1.4&uTime=0&wireframe=false"
+          urlString="https://www.shadergradient.co/customize?animate=on&axesHelper=off&bgColor1=%23000000&bgColor2=%23000000&brightness=0.35&cAzimuthAngle=180&cDistance=3.6&cPolarAngle=90&cameraZoom=1&color1=%23ff0700&color2=%233d9fdb&color3=%23d0bce1&embedMode=off&envPreset=city&fov=45&gizmoHelper=hide&grain=on&lightType=3d&pixelDensity=1&positionX=-1.4&positionY=0&positionZ=0&reflection=0.1&rotationX=0&rotationY=10&rotationZ=50&shader=defaults&type=waterPlane&uDensity=1.3&uFrequency=5.5&uSpeed=0.4&uStrength=1.4&uTime=0&wireframe=false"
         />
       </ShaderGradientCanvas>
 
