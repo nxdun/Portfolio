b18d8e614b3423bf69956fdb354e12dd0319ca88
Nadu_Dev <inbox.nadun@gmail.com>
2025-09-21 18:22:54 +0530

    Deployment fix (#8)
    * Update links in README.md to new domain
    
    * Update sitemap.xml to reflect new domain
    
    * Add Google Tag Manager integration
    
    * Update ChatBot configuration to use the new domain
    
    * feat: integrate Google Tag Manager for enhanced tracking
    
    * chore(readme) : best practices
    
    * bf : deploy fix

--- src/main.jsx
+++ src/main.jsx
@@ -3,18 +3,22 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
-// Google Tag Manager script
-(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-54K4GF5J');
+// Ensure Google Tag Manager script is added only once
+if (!document.querySelector('script[src*="googletagmanager"]')) {
+  (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+  new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+  'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+  })(window,document,'script','dataLayer','GTM-54K4GF5J');
+}
+
+// Ensure Google Tag Manager noscript is added only once
+if (!document.querySelector('noscript iframe[src*="googletagmanager"]')) {
+  const gtmNoscript = `<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-54K4GF5J" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>`;
+  document.body.insertAdjacentHTML('afterbegin', gtmNoscript);
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-
-// Add Google Tag Manager noscript
-const gtmNoscript = `<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-54K4GF5J" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>`;
-document.body.insertAdjacentHTML('afterbegin', gtmNoscript);
 
 // Check if running on localhost
 const isLocalhost = window.location.hostname === 'localhost' || 
@@ -30,14 +34,3 @@ if (isLocalhost) {
 } else {
   root.render(<App />)
 }
-
-// Add Google Tag Manager script to the head
-const gtmScript = document.createElement('script');
-gtmScript.async = true;
-gtmScript.src = 'https://www.googletagmanager.com/gtm.js?id=GTM-54K4GF5J';
-document.head.appendChild(gtmScript);
-
-// Add Google Tag Manager noscript to the body
-const gtmNoscript = document.createElement('noscript');
-gtmNoscript.innerHTML = `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-54K4GF5J" height="0" width="0" style="display:none;visibility:hidden"></iframe>`;
-document.body.insertBefore(gtmNoscript, document.body.firstChild);
