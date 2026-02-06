4322a525906a391659af202044b127ad98e13ab0
Nadu_Dev <inbox.nadun@gmail.com>
2026-02-06 15:17:50 +0530

    feat(docker): add Dockerfile and docker-compose for containerized development setup feat(config): add TypeScript configuration file fix(package): update version in package.json to 0.0.1

--- astro.config.ts
+++ astro.config.ts
@@ -10,6 +10,7 @@ import {
 } from "@shikijs/transformers";
 import { transformerFileName } from "./src/utils/transformers/fileName";
 import { SITE } from "./src/config";
+process.env.ASTRO_TELEMETRY_DISABLED = '1';
 
 // core from: https://astro.build/config
 export default defineConfig({
